26bca314cdc8a5ba6a25f00b00e6cbaa59f7fc49
janayia6632 <janayia6632@kippnyccp.org>
2017-07-25 10:34:00 -0400

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,24 @@
+int red = 7
+//int yellow =
+//int green =
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red, OUTPUT);
+  //pinMode(yellow, OUTPUT);
+  //pinMode(green, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(red, HIGH);
+  delay(1000);
+  digitalWrite(red, LOW);
+  //digitalWrite(yellow, HIGH);
+ // delay(1000)
+  //digitalWrite(yellow, LOW);
+  //digitalWrite(green,HIGH);
+  //delay(1000)
+  //digitalWrite(green, LOW)
+
+}
